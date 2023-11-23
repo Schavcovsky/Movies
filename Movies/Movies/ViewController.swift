f3413eef1e7f16f3523d73ba4cd3f22e5b9365d1
Alejandro Villalobos <alejandro.ignacio.villalobos@gmail.com>
2023-11-23 20:46:03 -0300

    Initial project setup

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Alejandro Villalobos on 23-11-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
